7f0c18d1356e9d70de235689f9084a0df058441e
User <User@WIN-Q06VGDO4GD9>
2021-12-20 19:36:54 +0300

    Добавьте файлы проекта.

---
+++ лаба№1.1/Linear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_1._1
+{
+    public class Linear
+    {
+        private double a;
+        private double b;
+        public double F (double x)
+        {
+            return A * x + B;
+        }
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+    }
+}
---
+++ лаба№1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linear obj = new Linear()
+            {
+                A = 5,
+                B = 8
+            };
+            Console.WriteLine($"f={obj.F(2)}");
+        }
+    }
+}
